d1dfdbf45d9a7aaccfd39dd1dd72629e24238ff9
Timothy Lin <55767165+Deodat-Lawson@users.noreply.github.com>
2021-09-29 22:40:58 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,20 @@
+//write my name
+//write my block
+
+/*
+
+instructions and stuff
+ */
+
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+	// write your code here
+
+        Calculator1 b = new Calculator1();
+        b.display();
+    }
+}
